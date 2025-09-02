8fd86ae85bf648da99107afda1f1389a730a8a67
FernandRios <fernandorios936@gmail.com>
2025-09-02 10:30:54 -0300

    movimento do player
    Está parcialmente correto apenas o player não está pulando irei olha mais tarde

---
+++ Jogo Ti/Policia3D/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public float movex = 1f;
+    public float jumpforce = 5f;
+    public float verticalMultiplier = 2f;
+
+    private Rigidbody rb;
+    private bool isground;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+
+        float movey = -Input.GetAxis("Horizontal") * verticalMultiplier;
+        // float movey = Input.GetAxis("Vertical") * verticalMultiplier; 
+
+        Vector3 movement = new Vector3(movex, 0, movey) * speed * Time.deltaTime;
+
+        Vector3 newPosition = rb.position + movement;
+
+        rb.MovePosition(newPosition);
+
+        if (Input.GetKeyDown(KeyCode.Backspace) && isground)
+        {
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            isground = false;
+        }
+    }       
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("chao"))
+        {
+            isground = true;
+            Debug.Log("Está no chão");
+        }
+
+    }
+}
