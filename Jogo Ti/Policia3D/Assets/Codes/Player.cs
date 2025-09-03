3da18a4b7a7b73868b413768c7ec97c32c27a4f3
Lucas de Lima <lucaslsilva64@gmail.com>
2025-09-03 17:25:03 -0300

    Todas as mudanças feita na aula
    -Colocar assets para dar uma sensação de mov
    -aquele quadridos.

--- Jogo Ti/Policia3D/Assets/Codes/Player.cs
+++ Jogo Ti/Policia3D/Assets/Codes/Player.cs
@@ -34,6 +34,7 @@ public class Player : MonoBehaviour
     {
         if(collision.gameObject.tag == "Obstacle")
         {
+            Time.timeScale = 0;
             GameOver.instacia.GameOverScreen();
         }
     }
